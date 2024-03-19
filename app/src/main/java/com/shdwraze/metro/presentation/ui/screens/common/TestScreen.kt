6ee6afeaf2098023d08eb9575eca5b65ff89e0fc
shdwraze <shdwrazedev@gmail.com>
2024-03-19 20:53:36 +0200

    sidebar in animation

--- app/src/main/java/com/shdwraze/metro/presentation/ui/screens/common/TestScreen.kt
+++ app/src/main/java/com/shdwraze/metro/presentation/ui/screens/common/TestScreen.kt
@@ -2,12 +2,13 @@ package com.shdwraze.metro.presentation.ui.screens.common
 
 import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.expandVertically
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.animation.shrinkVertically
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -29,16 +30,15 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.material3.rememberStandardBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
@@ -143,30 +143,51 @@ fun TestScreen(
         SidebarMenu(
             onCloseClick = {
                 isMenuOpened = false
-            }
+            },
+            visible = isMenuOpened
         )
     }
 }
 
 @Composable
 fun SidebarMenu(
-    onCloseClick: () -> Unit = {}
+    onCloseClick: () -> Unit = {},
+    visible: Boolean
 ) {
+    val visibleState = remember { mutableStateOf(false) }
+
+    LaunchedEffect(visible) {
+        visibleState.value = visible
+    }
+
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(color = Color.Black.copy(alpha = 0.5f))
             .noRippleClickable { onCloseClick() }
     ) {
-        Box(
-            modifier = Modifier
-                .fillMaxHeight()
-                .width(300.dp)
-                .background(color = MaterialTheme.colorScheme.onPrimary)
-                .noRippleClickable { }
-                .padding(16.dp)
+        AnimatedVisibility(
+            visible = visibleState.value,
+            enter = slideInHorizontally(
+                animationSpec = tween(durationMillis = 100),
+                initialOffsetX = { fullWidth -> -fullWidth / 3 }
+            ) + fadeIn(animationSpec = tween(durationMillis = 100)),
+            exit = slideOutHorizontally(
+                animationSpec = spring(stiffness = Spring.StiffnessHigh),
+                targetOffsetX = { 100 }
+            ) + fadeOut()
         ) {
-            Text(text = "Coming soon")
+            Log.d("TEST", "I'm here")
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .width(300.dp)
+                    .background(color = MaterialTheme.colorScheme.onPrimary)
+                    .noRippleClickable { }
+                    .padding(16.dp)
+            ) {
+                Text(text = "Coming soon")
+            }
         }
     }
 }
